67ef09d1c9894b5df010b168d75480d096fcc34a
Joe Pentland <joe.pentland@gmail.com>
2015-11-17 14:04:40 +0100

    deploy.sh: Also link xmobarrc

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 function delete_and_link {
 	SOURCE=$1
 	DEST=$2
-	rm -i $HOME/$DEST
-	ln -s $PWD/SOURCE $HOME/DEST
+	rm -i "$HOME/$DEST"
+	ln -s "$PWD/$SOURCE" "$HOME/$DEST"
 }
 
 delete_and_link "bashrc"	".bashrc"
 delete_and_link "vimrc"		".vimrc"
 delete_and_link "xmonad.hs"	".xmonad/xmonad.hs"
+delete_and_link "xmobarrc"	".xmobarrc"
