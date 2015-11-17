830cd8b1457967f602c95f787e3a3cd412325253
Joe Pentland <joe.pentland@gmail.com>
2015-11-17 17:02:48 +0100

    deploy.sh: Add comment header for delete_and_link

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,11 @@ function delete_and_link {
 	ln -s "$PWD/$CFGDIR/$SOURCE" "$HOME/$DEST"
 }
 
-delete_and_link "bashrc"	".bashrc"
-delete_and_link "vimrc"		".vimrc"
-delete_and_link "xmonad.hs"	".xmonad/xmonad.hs"
-delete_and_link "xmobarrc"	".xmobarrc"
-delete_and_link "halp"		".halp"
-delete_and_link "bashrc.d"	".bashrc.d"
-delete_and_link "gitconfig"	".gitconfig"
+#		Source filename		# Destination filebane (relative to $HOME)
+delete_and_link "bashrc"		".bashrc"
+delete_and_link "vimrc"			".vimrc"
+delete_and_link "xmonad.hs"		".xmonad/xmonad.hs"
+delete_and_link "xmobarrc"		".xmobarrc"
+delete_and_link "halp"			".halp"
+delete_and_link "bashrc.d"		".bashrc.d"
+delete_and_link "gitconfig"		".gitconfig"
