1fb26a9169bc96c40d0d238fa5d181331ae1b9b3
Joe Pentland <joe.pentland@gmail.com>
2015-11-17 13:45:28 +0100

    deploy.sh: Also link vimrc

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-rm -i ~/.bashrc
-ln -s $PWD/bashrc $HOME/.bashrc
+
+function delete_and_link {
+	SOURCE=$1
+	DEST=$2
+	rm -i $HOME/$DEST
+	ln -s $PWD/SOURCE $HOME/DEST
+}
+
+delete_and_link bashrc .bashrc
+delete_and_link vimrc .vimrc
