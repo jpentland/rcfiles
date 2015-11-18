dcee700ec246e7500a3014df1de91eb5cdf524bd
Joe Pentland <joe.pentland@gmail.com>
2015-11-18 17:47:01 +0100

    Split off opsy-specific stuff

--- configs/bashrc.d/70-env.sh
+++ configs/bashrc.d/70-env.sh
@@ -1,12 +1,4 @@
 # Environment Variables
-export SYSGO_LICENSE_PATH=6200@license.open-synergy.com
-export PATH=$PATH:/opt/codeo-4.0/bin
-export PATH=$PATH:/opt/elinos-5.2/bin
-export PATH=$PATH:/sbin
-export PATH=$PATH:~/script
 export PATH=~/.local/bin:$PATH
 export EDITOR=vim
-export COBASEDIR=~/foscm
-export TARGETDIR=/media/8bde808a-be9e-4118-9b27-ed627512b9f9
-export GITWEB=https://git.opensynergy.com/G/
 export BROWSER=chromium-browser
