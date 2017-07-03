4d658e93f74b8e450f7a10e226c99d3a6a557900
Joe Pentland <joe.pentland@gmail.com>
2017-07-03 11:45:51 +0200

    bashrc.d: Update to new COQOS_LICENSE_FILE in 71-env-opsy.sh

--- configs/bashrc.d/71-env-opsy.sh
+++ configs/bashrc.d/71-env-opsy.sh
@@ -5,4 +5,4 @@ export PATH=$PATH:/opt/elinos-5.2/bin
 export PATH=$PATH:/sbin
 export COBASEDIR=~/foscm
 export GITWEB=https://git.opensynergy.com/G/
-export COQOS_LICENSE_FILE=$(pwd)/coqos.lic
+export COQOS_LICENSE_FILE=~/coqos-license/coqos.lic
