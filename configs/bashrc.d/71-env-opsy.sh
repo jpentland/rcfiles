aa0fbbbee76070c032bab130a7ad6169c7f3bd48
Joe Pentland <joe.pentland@gmail.com>
2017-09-15 16:27:25 +0200

    bashrc.d: 71-env-opsy.sh: Add JAVA_HOME=
    Because graphics drivers for R-Carh3 on android fail to build otherwise

--- configs/bashrc.d/71-env-opsy.sh
+++ configs/bashrc.d/71-env-opsy.sh
@@ -4,4 +4,5 @@ export PATH=$PATH:/sbin
 export COBASEDIR=~/foscm
 export GITWEB=https://git.opensynergy.com/G/
 export COQOS_LICENSE_FILE=~/coqos-license/coqos.lic
+export JAVA_HOME=
 export XCHANGE=/media/mightfreeze/XChange/
