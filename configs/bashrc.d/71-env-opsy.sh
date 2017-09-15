b86355ea7ceefc32ee22b869fe1d8743956b5e86
Joe Pentland <joe.pentland@gmail.com>
2017-09-15 16:26:48 +0200

    bashrc.d: 71-env-opsy: Add XCHANGE dir

--- configs/bashrc.d/71-env-opsy.sh
+++ configs/bashrc.d/71-env-opsy.sh
@@ -1,8 +1,7 @@
 # Environment Variables
 export SYSGO_LICENSE_PATH=6200@license.open-synergy.com
-export PATH=$PATH:/opt/codeo-4.0/bin
-export PATH=$PATH:/opt/elinos-5.2/bin
 export PATH=$PATH:/sbin
 export COBASEDIR=~/foscm
 export GITWEB=https://git.opensynergy.com/G/
 export COQOS_LICENSE_FILE=~/coqos-license/coqos.lic
+export XCHANGE=/media/mightfreeze/XChange/
