727ba50b61cc1537bbfe6715f870bbff4d0d3d36
Joe Pentland <joe.pentland@gmail.com>
2018-06-19 11:11:42 +0200

    70-env.sh: Add colour to less and man pages

--- configs/bashrc.d/70-env.sh
+++ configs/bashrc.d/70-env.sh
@@ -8,3 +8,11 @@ shopt -s histappend
 # make grep highlight results using color
 export GREP_OPTIONS='--color=auto'
 
+# Add some colour to LESS/MAN pages
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;33m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m' 
+export LESS_TERMCAP_so=$'\E[01;42;30m'
+export LESS_TERMCAP_ue=$'\E[0mkkkk'
+export LESS_TERMCAP_us=$'\E[01;36m'
