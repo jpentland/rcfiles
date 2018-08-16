d71ba2f458731c5fb664423757b76a705f3b711c
Joe Pentland <joe.pentland@gmail.com>
2018-08-16 13:06:52 +0200

    bashrc.d: Delete opsy-specific files

--- configs/bashrc.d/71-env-opsy.sh
+++
@@ -1,8 +0,0 @@
-# Environment Variables
-export SYSGO_LICENSE_PATH=6200@license.open-synergy.com
-export PATH=$PATH:/sbin
-export COBASEDIR=~/foscm
-export GITWEB=https://git.opensynergy.com/G/
-export COQOS_LICENSE_FILE=~/coqos-license/coqos.lic
-export JAVA_HOME=
-export XCHANGE=/media/mightfreeze/XChange/
--- configs/bashrc.d/81-alias-opsy.sh
+++
@@ -1,3 +0,0 @@
-# Aliases
-alias foscm=/home/jpe/apps/foscm/foscm
-alias curl-os="curl --cert $CD/cert.crt  --key $CD/key.pem --cacert $CD/cacert.crt"
