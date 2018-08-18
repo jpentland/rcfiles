d594fa34824e927390550f3e88d03b8a27e21bf8
Joseph Pentland <joe.pentland@gmail.com>
2018-08-18 14:57:09 +0200

    bashrc.d: 20-color-ls.sh: Use exa for 'l' if available, otherwise use ls.

--- configs/bashrc.d/20-color-ls.sh
+++ configs/bashrc.d/20-color-ls.sh
@@ -13,5 +13,8 @@ fi
 # some more ls aliases
 alias ll='ls -alF'
 alias la='ls -A'
-alias l='ls -Cl'
+alias l='ls-l'
 alias lh='ls -alFh'
+
+# Use 'exa' for 'l' if available
+which exa && alias l='exa -l'
