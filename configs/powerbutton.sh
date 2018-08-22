496f6e582b34e4b3d70cea3efe662ef4dff7601e
Joe Pentland <joe.pentland@gmail.com>
2018-08-22 17:26:08 +0200

    powerbutton.sh: Improve handling of restarts / interrupts

--- configs/powerbutton.sh
+++ configs/powerbutton.sh
@@ -5,14 +5,20 @@ function ask {
 	echo "yad --center --image \"dialog-question\" --title \"Alert\" --button=gtk-yes:\"$2\" --button=gtk-no:1 --text \"$1?\""
 }
 
+function cleanup {
+	kill $(cat $TMP_FILE); rm -f $TMP_FILE
+}
+
 menu="\
 Suspend!bash -c 'slock & systemctl suspend'\
 |Shut Down!$(ask 'Shut Down' 'systemctl poweroff')\
 |Restart!$(ask 'Restart' 'systemctl reboot')\
 "
 
-if [ -f $TMP_FILE ]; then exit 0; fi
+if ps -A | grep $(cat $TMP_FILE); then exit 0; fi
 
-touch $TMP_FILE
-yad --notification --menu="$menu" --image="system-shutdown"  --command=
-rm $TMP_FILE
+yad --notification --menu="$menu" --image="system-shutdown"  --command= &
+echo $! > $TMP_FILE
+trap 'echo recieved SIGINT; cleanup;' SIGINT
+wait
+cleanup
