861f9863f985be98d3efd508fcd85267d2443f1f
Joe Pentland <joe.pentland@gmail.com>
2018-09-11 19:24:39 +0200

    70-env.sh: Remove trailing space character.

--- configs/bashrc.d/70-env.sh
+++ configs/bashrc.d/70-env.sh
@@ -10,7 +10,7 @@ shopt -s histappend
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;33m'
 export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m' 
+export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;42;30m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;36m'
