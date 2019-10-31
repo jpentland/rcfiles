a616fa8d40ade620d77d72c1279eff5449d3a791
Joe Pentland <joe.pentland@gmail.com>
2019-10-31 20:23:17 +0100

    70-env.sh: Add z for fast moving

--- configs/bashrc.d/70-env.sh
+++ configs/bashrc.d/70-env.sh
@@ -15,3 +15,5 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;42;30m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;36m'
+
+[[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh
