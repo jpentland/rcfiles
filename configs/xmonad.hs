b40c1e0c13607ed539da571c9e062c7ad2238d5f
Joseph Pentland <joe.pentland@gmail.com>
2019-11-25 12:22:16 +0100

    xmonad.hs: Use st for terminal

--- configs/xmonad.hs
+++ configs/xmonad.hs
@@ -49,7 +49,7 @@ main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
 	{ borderWidth	= 1
-	, terminal	= "konsole"
+	, terminal	= "st"
 	, normalBorderColor = "#444444"
 	, focusedBorderColor = "#8888dd"
 	, keys          = \c -> mykeys c `M.union` keys defaultConfig c
