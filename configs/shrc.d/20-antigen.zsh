4be8a6727291f15620b0c8ee7805bb90421154ce
Joe Pentland <joe.pentland@gmail.com>
2019-11-25 15:43:59 +0100

    antigen: Add support for history substring search

--- configs/shrc.d/20-antigen.zsh
+++ configs/shrc.d/20-antigen.zsh
@@ -12,12 +12,16 @@ antigen bundle pip
 antigen bundle lein
 antigen bundle command-not-found
 antigen bundle zsh-users/zsh-autosuggestions
-
-# Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
 
 # Load the theme.
 antigen theme robbyrussell
 
 # Tell Antigen that you're done.
 antigen apply
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
