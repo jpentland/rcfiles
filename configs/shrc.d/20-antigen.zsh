c3f4504597e4973bbf6e5ed888fe9405651f0b20
Joe Pentland <joe.pentland@gmail.com>
2019-11-27 09:08:42 +0100

    antigen: Remove lein plugin

--- configs/shrc.d/20-antigen.zsh
+++ configs/shrc.d/20-antigen.zsh
@@ -9,7 +9,6 @@ antigen use oh-my-zsh
 antigen bundle git
 antigen bundle heroku
 antigen bundle pip
-antigen bundle lein
 antigen bundle command-not-found
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle zsh-users/zsh-syntax-highlighting
