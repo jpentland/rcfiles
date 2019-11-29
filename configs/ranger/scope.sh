1e574748047023bebec9a6fb2f573e50dffaf936
Joe Pentland <joe.pentland@gmail.com>
2019-11-29 13:18:03 +0100

    ranger: scope.sh: Use bat for text previews (enables git integration)

--- configs/ranger/scope.sh
+++ configs/ranger/scope.sh
@@ -179,8 +179,11 @@ handle_mime() {
                 local pygmentize_format='terminal'
                 local highlight_format='ansi'
             fi
-            highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
-                --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
+            #highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
+            #    --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}" && exit 5
+#            COLORTERM=8bit bat "${FILE_PATH}" --color always
+            env COLORTERM=8bit bat --style="changes" --color=always "${FILE_PATH}" && exit 5
+#
             # pygmentize -f "${pygmentize_format}" -O "style=${PYGMENTIZE_STYLE}" -- "${FILE_PATH}" && exit 5
             exit 2;;
 
