3fb722eb8efb9e97a4cdeaf51a52ad24e2fa0f42
Joe Pentland <joe.pentland@gmail.com>
2019-12-03 16:19:36 +0100

    zsh: prompt: Use a line bar when in insert mode

--- configs/shrc.d/90-prompt.zsh
+++ configs/shrc.d/90-prompt.zsh
@@ -1,2 +1,25 @@
 which starship 2>&1 > /dev/null && eval "$(starship init zsh)"
 set -o vi
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+_fix_cursor() {
+   echo -ne '\e[5 q'
+}
+precmd_functions+=(_fix_cursor)
