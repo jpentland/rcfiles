75c3bdff569193f210c94b8a17c9dab6e0dcc302
Joseph Pentland <joe.pentland@gmail.com>
2019-12-05 23:12:26 +0100

    bluetooth: remove line bars

--- configs/xmobar/bluetooth.sh
+++ configs/xmobar/bluetooth.sh
@@ -3,7 +3,7 @@ alias bt=bluetoothctl
 bt devices | \
 	while read device id name; do
 		if [ "$(bt info $id | awk '/Connected/{print $2}')" == "yes" ]; then
-			echo -n -e "\xef\x80\xa5 $name | "
+			echo -n -e "\xef\x80\xa5 $name "
 		fi
 	done | \
-		sed 's/ | $//'
+		sed 's/ $//'
