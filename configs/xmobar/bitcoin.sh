cae194adf2d6c1eb5989667b40d788b674466d93
Joseph Pentland <joe.pentland@gmail.com>
2019-12-05 23:12:01 +0100

    bitcoin: Change color depending on 24h change

--- configs/xmobar/bitcoin.sh
+++ configs/xmobar/bitcoin.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-set -xe
 json=$(curl -s http://api.coindesk.com/v1/bpi/currentprice/USD.json)
-ratefloat=$(echo $json | jq '.bpi .USD .rate_float')
-rate=$(printf "%.0f" $ratefloat)
+rate=$(echo $json | jq '.bpi .USD .rate_float')
+intrate=$(printf "%.0f" $rate)
 
-yesterday=$(date -d "yesterday 13:00" "+%Y-%m-%d")
-yesterdayjson=$(curl -s "https://api.coindesk.com/v1/bpi/historical/close.json?start=$yesterday&end=$yesterday")
-yesterdayratefloat=$(echo "$yestedayjson" | jq ".bpi .\"$yesterday\"")
+yd=$(date -d "yesterday 13:00" "+%Y-%m-%d")
+ydjson=$(curl -s "https://api.coindesk.com/v1/bpi/historical/close.json?start=$yd&end=$yd")
+ydrate=$(echo "$ydjson" | jq ".bpi .\"$yd\"")
+diff=$(printf "%.2f" $(bc -l <<< "(1 - ( $ydrate / $rate ) ) * 100"))
+diffint=$(printf "%d" $(bc -l <<< "$diff * 100"))
 
-diff=$(printf "%.2f" $(bc -l <<< "(1 - ( $yesterdayratefloat / $ratefloat ) ) * 100"))
-echo -n -e "\xef\x85\x9a = \$$rate $diff"
+bticon="<fc=#ffcc00>\xef\x85\x9a</fc>"
+
+if [ "$diffint" -lt "0" ]; then
+	echo -n -e "$bticon = <fc=#ff0000>\$$intrate</fc>"
+else
+	echo -n -e "$bticon = <fc=#00ff00>\$$intrate</fc>"
+fi
