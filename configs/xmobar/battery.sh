247de00cab81a1bda66922c43cfec4b4ba2125fb
Joseph Pentland <joe.pentland@gmail.com>
2019-12-05 23:11:34 +0100

    battery: Use color and fix getting device names

--- configs/xmobar/battery.sh
+++ configs/xmobar/battery.sh
@@ -6,24 +6,24 @@ low=""
 empty=""
 powered=""
 
-battery="/org/freedesktop/UPower/devices/battery_BAT0"
-power="/org/freedesktop/UPower/devices/line_power_AC"
+battery=$( upower -e | grep battery | sed 1q )
+power=$(upower -e | grep 'line_power' | sed 1q)
 
 charge=$(upower -i $battery | awk '/percentage/{print $2}' | sed 's/%//')
 online=$(upower -i $power | awk '/online/{print $2}')
 
 if [ "$online" == "yes" ]; then
-	echo -n -e "$powered "
+	echo -n -e "<fc=#ffcc00>$powered</fc> "
 fi
 
 if [ "$charge" -gt 95 ]; then
-	echo -n -e "$full"
+	echo -n -e "<fc=#00ff00>$full</fc>"
 elif [ "$charge" -gt 75 ]; then
 	echo -n -e "$high"
 elif [ "$charge" -gt 50 ]; then
 	echo -n -e "$medium"
 elif [ "$charge" -gt 10 ]; then
-	echo -n -e "$low"
+	echo -n -e "<fc=#ff0000>$low</fc>"
 else
 	echo -n -e "$empty"
 fi
