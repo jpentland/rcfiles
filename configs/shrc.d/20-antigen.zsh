74b9cca9b9dc99f408d0f3f43c8aea3a9c16cc7e
Joseph Pentland <joe.pentland@gmail.com>
2019-12-13 01:58:06 +0100

    antigen: Remove heroku plugin

--- configs/shrc.d/20-antigen.zsh
+++ configs/shrc.d/20-antigen.zsh
@@ -7,7 +7,6 @@ antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
 antigen bundle git
-antigen bundle heroku
 antigen bundle pip
 antigen bundle command-not-found
 antigen bundle gradle
