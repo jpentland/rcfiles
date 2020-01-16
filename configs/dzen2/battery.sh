51a976e031f943c789c3c24d85b8b9df9e93cd0f
Joe Pentland <joe.pentland@gmail.com>
2020-01-16 12:29:21 +0100

    battery.sh: More coloring, and don't  display battery icon if fully charged and powered

--- configs/dzen2/battery.sh
+++ configs/dzen2/battery.sh
@@ -12,19 +12,31 @@ power=$(upower -e | grep 'line_power' | sed 1q)
 charge=$(upower -i $battery | awk '/percentage/{print $2}' | sed 's/%//')
 online=$(upower -i $power | awk '/online/{print $2}')
 
-if [ "$online" == "yes" ]; then
-	echo -n -e "^fg(#ffcc00)$powered^fg() "
+if [ "$charge" -gt 95 ]; then
+	icon=$full
+	color="#00ff00"
+elif [ "$charge" -gt 75 ]; then
+	icon=$high
+	color="#22ff22"
+elif [ "$charge" -gt 40 ]; then
+	icon=$medium
+	color="#ff0088"
+elif [ "$charge" -gt 10 ]; then
+	icon="$low ($charge)"
+	color="#ff0000"
+else
+	icon="$empty ($charge)"
+	color="#ff0000"
 fi
 
-if [ "$charge" -gt 95 ]; then
-	echo -n -e "^fg(#00ff00)$full^fg()"
-elif [ "$charge" -gt 75 ]; then
-	echo -n -e "$high"
-elif [ "$charge" -gt 40 ]; then
-	echo -n -e "$medium"
-elif [ "$charge" -gt 10 ]; then
-	echo -n -e "^fg(#ff0000)$low $charge%^fg()"
+if [ "$online" == "yes" ]; then
+	echo -n -e "^fg(#ffcc00)$powered^fg() "
+	if [ "$charge" -lt "99" ]; then
+		echo -n -e "^fg(#00ff00)$icon^fg()"
+	fi
 else
-	echo -n -e "^fg(#ff0000)$empty $charge%^fg()"
+	echo -n -e "^fg($color)$icon^fg()"
 fi
+
+
 echo
