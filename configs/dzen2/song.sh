cb6a64f427e4c00066480d72d979d244a954f392
Joe Pentland <joe.pentland@gmail.com>
2020-01-16 12:30:07 +0100

    song.sh: Improve spacing

--- configs/dzen2/song.sh
+++ configs/dzen2/song.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 icon="\uf001"
 if [ "$(playerctl status)" != "Playing" ]; then
+	echo
 	exit 0
 fi
 
 data="$(playerctl metadata)"
 title=$(echo "$data" | sed -n 's/.*:title \+\(.*\)/\1/p')
 artist=$(echo "$data" | sed -n 's/.*:artist \+\(.*\)/\1/p')
-echo -e " $icon $title - $artist"
+echo -e " $icon $title - $artist "
