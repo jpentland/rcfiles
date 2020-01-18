dff4a257c9183e0bd9fb2504df632c0111f0fcc6
Joe Pentland <joe.pentland@gmail.com>
2020-01-18 20:35:19 +0100

    70-env: Add XDG_CONFIG_HOME definition

--- configs/shrc.d/70-env.sh
+++ configs/shrc.d/70-env.sh
@@ -7,6 +7,7 @@ export GOPATH=~/.gopath
 export PATH=$GOPATH/bin:/opt/google-cloud-sdk/platform/google_appengine/:$PATH
 export PATH=$HOME/.local/scripts:$PATH
 export shell=$(sh -c 'ps -p $$ -o ppid=' | xargs ps -o comm= -p)
+export XDG_CONFIG_HOME="$HOME/.config"
 
 # Add some colour to LESS/MAN pages
 export LESS_TERMCAP_mb=$'\E[01;31m'
