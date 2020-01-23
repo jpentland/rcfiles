8550e5410ef911c5ecd0c5b4b9e4f50566c6f924
Joe Pentland <joe.pentland@gmail.com>
2020-01-23 20:35:10 +0100

    dropdown.sh: Auto-detect padding and fix moving between desktops

--- scripts/dropdown.sh
+++ scripts/dropdown.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -x
+#!/bin/sh
 
-# Usage: dropdown.sh [--role <role>] <spawn command> <class> <posx> <posy> <sizex> <sizey> <yoff>
+# Usage: dropdown.sh [--role <role>] <spawn command> <class> <posx> <posy> <sizex> <sizey>
 if [ "$1" == "--role" ]; then
         shift
         role="$1"; shift
@@ -18,9 +18,6 @@ PY=$1; shift
 # Size in percentage of monitor size
 SX=$1; shift
 SY=$1; shift
-
-# Y Offset in absolute pixels (to avoid bar)
-YOFF=$1; shift
 
 # Get role of a wid
 function get_role {
@@ -42,14 +39,14 @@ function get_wids {
 }
 
 # Calculate size in pixels
-read screen_width screen_height screen_x screen_y borderWidth \
+read screen_width screen_height screen_x screen_y borderWidth top_padding \
         <<< "$(bspc query -T -m focused \
-        | jq '.rectangle.width, .rectangle.height, .rectangle.x, .rectangle.y, .borderWidth' \
+        | jq '.rectangle.width, .rectangle.height, .rectangle.x, .rectangle.y, .borderWidth, .padding.top' \
         | tr '\n' ' ')"
 RPX=$(bc <<< "($screen_width * $PX) / 100 + $screen_x")
-RPY=$(bc <<< "($screen_height * $PY) / 100 + $YOFF + $screen_y")
+RPY=$(bc <<< "($screen_height * $PY) / 100 + $top_padding + $screen_y")
 RSX=$(bc <<< "($screen_width * $SX) / 100 - ($borderWidth * 2)")
-RSY=$(bc <<< "(($screen_height - $YOFF) * $SY) / 100 - ($borderWidth * 2)")
+RSY=$(bc <<< "(($screen_height - $top_padding) * $SY) / 100 - ($borderWidth * 2)")
 
 # Spawn window if it does not exist
 wids=$(get_wids)
@@ -68,7 +65,8 @@ fi
 
 # Move window into place before showing it
 for wid in $wids; do
-        if [ -z "$(bspc query -N -n $wid.\!hidden -d focused -m focused)" ]; then
+        echo $(bspc query -N -n $wid.\!hidden.local) >> /dev/stderr
+        if [ -z "$(bspc query -N -n $wid.\!hidden.local)" ]; then
                 xdotool windowmove $wid $RPX $RPY
                 xdotool windowsize $wid $RSX $RSY
                 bspc node $wid -g hidden=off -d focused -f -t floating
