b9c3ee8ae0d00477ca78ffc8999c9fb9e463622f
Joe Pentland <joe.pentland@gmail.com>
2020-02-14 14:19:45 +0100

    zsh: Remove ohmyzsh and git plgin

--- configs/shrc.d/20-antigen.zsh
+++ configs/shrc.d/20-antigen.zsh
@@ -2,11 +2,7 @@ ANTIGEN="$HOME/antigen.zsh"
 [ -f $ANTIGEN ] || wget -c git.io/antigen -O $ANTIGEN
 source $ANTIGEN
 
-# Load the oh-my-zsh's library.
-antigen use oh-my-zsh
-
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle git
 antigen bundle pip
 antigen bundle command-not-found
 antigen bundle gradle
