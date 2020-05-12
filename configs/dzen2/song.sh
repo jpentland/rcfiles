171987a30b4a43965b3c634f46e49a94cdf3b2cb
Joe Pentland <joe.pentland@gmail.com>
2020-05-12 14:50:49 +0200

    song.sh: Display paused icon if music is paused

--- configs/dzen2/song.sh
+++ configs/dzen2/song.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-icon="\uf001"
-if [ "$(playerctl status)" != "Playing" ]; then
+playing="\uf001"
+paused="\uf04c"
+status=$(playerctl status)
+if grep -q "Playing" <<< "$status"; then
+	icon=$playing
+	data="$(playerctl metadata)"
+	title=$(echo "$data" | sed -n 's/.*:title \+\(.*\)/\1/p')
+	artist=$(echo "$data" | sed -n 's/.*:artist \+\(.*\)/\1/p')
+	echo -e " $icon $title - $artist "
+elif grep -q "Paused" <<< "$status"; then
+	icon=$paused
+	echo -e " $icon"
+else
 	echo
 	exit 0
 fi
 
-data="$(playerctl metadata)"
-title=$(echo "$data" | sed -n 's/.*:title \+\(.*\)/\1/p')
-artist=$(echo "$data" | sed -n 's/.*:artist \+\(.*\)/\1/p')
-echo -e " $icon $title - $artist "
